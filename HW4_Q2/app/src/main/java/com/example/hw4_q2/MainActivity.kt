92b7964f697cb1e344a2fdeb7663d64c74de36e2
441gabe <guillermo.gabe@gmail.com>
2023-10-18 20:06:12 -0400

    Added functionality for question 2

--- HW4_Q2/app/src/main/java/com/example/hw4_q2/MainActivity.kt
+++ HW4_Q2/app/src/main/java/com/example/hw4_q2/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.hw4_q2
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import java.util.Objects
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sqrt
 
 
 /*
@@ -16,37 +24,77 @@ if the accelerometer records a large difference in a set amount of seconds, then
  */
 
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var sensorManager: SensorManager
     private lateinit var sensitivityBar: SeekBar
     private lateinit var textView: TextView
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         sensitivityBar = findViewById(R.id.sensitivityBar)
-        textView = findViewById(R.id.sensitivityBar)
+        textView = findViewById(R.id.textView)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager).registerListener(sensorListener,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
 
 
 
     }
+    private val sensorListener: SensorEventListener = object : SensorEventListener{
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            val gravity = 9.81
+            lastAcceleration = currentAcceleration
+            val accelerationX = abs(x)
+            val accelerationY = abs(y - 9.81).toFloat()
+            val accelerationZ = abs(z)
+//            Log.d(TAG, x.toString())
+//            Log.d(TAG, y.toString())
+//            Log.d(TAG, z.toString())
 
-    private fun initSensor(){
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
-            sensorManager.registerListener(this,
-                it,
-                SensorManager.SENSOR_DELAY_FASTEST,
-                SensorManager.SENSOR_DELAY_FASTEST )
+
+            val maxAxis = Math.max(accelerationX, Math.max(accelerationY, accelerationZ))
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            //val difference: Float = currentAcceleration - lastAcceleration
+            //acceleration = acceleration * 0.9f + difference
+            val xToast = Toast.makeText(this@MainActivity, "X Axis!", Toast.LENGTH_SHORT)
+            val yToast = Toast.makeText(this@MainActivity, "Z Axis!", Toast.LENGTH_SHORT)
+            val zToast = Toast.makeText(this@MainActivity, "Z Axis!", Toast.LENGTH_SHORT)
+
+            if(maxAxis > (sensitivityBar.progress + 1) * 2){
+
+                when (maxAxis){
+                    accelerationX -> {
+                        Log.d(TAG, "X Axis")
+                        xToast.show()
+                    }
+                    accelerationY -> {
+                        Log.d(TAG, "Y Axis")
+                        yToast.show()
+                    }
+                    accelerationZ -> {
+                        Log.d(TAG, "Z Axis")
+                        zToast.show()
+
+                        val x = zToast.view!!.isShown
+                        }
+                    else -> Log.d(TAG, "Error!")
+                }
+            }
+
         }
-    }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
     }
 }
