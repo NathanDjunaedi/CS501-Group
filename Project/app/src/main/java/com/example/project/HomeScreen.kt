c1c8ee3654ee711581a9c28670631e2a2b07f505
441gabe <guillermo.gabe@gmail.com>
2023-12-21 15:32:16 -0500

    Added to Home Screen

--- Project/app/src/main/java/com/example/project/HomeScreen.kt
+++ Project/app/src/main/java/com/example/project/HomeScreen.kt
@@ -1,14 +1,54 @@
 package com.example.project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeScreen :AppCompatActivity() {
+    // Button Variables
+    private lateinit var newEntryButton: Button
+    private lateinit var settingsButton: Button
+
+    // RecyclerView Variables
+    private lateinit var entriesReycler: RecyclerView
+
+    // TextView Variables
+    private lateinit var radiusTextView: TextView
+
+    // Seekbar Variables
+    private lateinit var radiusSeekBar: SeekBar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen)
 
+        // Setting variables for buttons
+        newEntryButton = findViewById(R.id.add_entry_main_screen)
+        settingsButton = findViewById(R.id.settings_main_screen)
 
+        // Setting up variable for recycler view
+        entriesReycler = findViewById(R.id.entries_main_screen)
 
+        // Setting up variables for seekbar / textview
+        radiusTextView = findViewById(R.id.radius_main_screen)
+        radiusSeekBar = findViewById(R.id.radius_main_screen)
+
+        // Listeners for buttons
+        newEntryButton.setOnClickListener { goToNewEntry() }
+        settingsButton.setOnClickListener { goToSettings() }
+    }
+
+    private fun goToSettings() {
+        // Going to settings activity
+        startActivity(Intent(this, Settings::class.java))
+    }
+    private fun goToNewEntry() {
+        // Going to new entry activity
+        startActivity(Intent(this, NewEntry::class.java))
     }
 }
